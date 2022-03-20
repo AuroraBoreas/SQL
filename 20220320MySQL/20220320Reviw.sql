a0166e74ff7f0e0c974b7503742fa5395e7293fb
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2022-03-20 23:39:02 +0800

    Add files via upload

--- 20220320MySQL/20220320Reviw.sql
+++ 20220320MySQL/20220320Reviw.sql
@@ -32,3 +32,30 @@ VALUES
     ('2014/12/12', 800.67, 2),
     ('2015/01/03', 12.5, 2),
     ('1999/04/11', 450.25, 5);
+
+-- cross product
+-- all in one:: implicit inner join
+SELECT * FROM customers, orders;
+
+-- filter
+SELECT * FROM customers, orders
+WHERE customers.customer_id = orders.customer_id;
+
+-- refine
+SELECT first_name, last_name, order_date, amount
+FROM customers, orders
+WHERE customers.customer_id = orders.customer_id;
+
+-- explicit inner join
+SELECT first_name, last_name, order_date, amount
+FROM customers
+JOIN orders
+ON customers.customer_id = orders.customer_id;
+
+-- fancier
+SELECT first_name, last_name, SUM(amount) AS total_spent
+FROM customers
+INNER JOIN orders
+ON customers.customer_id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY total_spent;
